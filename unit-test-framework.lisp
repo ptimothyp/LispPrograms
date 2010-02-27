d08c8bc34461002e4a02141d04634d65651375f0
Timothy Pakkianathan <ptimothyp@gmail.com>
2010-02-27 12:09:16 +1100

    Unit test till 'check' duplication

--- unit-test-framework.lisp
+++ unit-test-framework.lisp
@@ -1,1 +1,10 @@
-ls
+(defun test-+ ()
+  (check (= (+ 1 2) 3))
+  (check (= (+ 1 2 3) 6))
+  (check (= (+ -1 -5) -6)))
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] .... ~a~%" result form))
+
+(defmacro check(form)
+  `(report-result ,form ',form))
