461414c6a111110bc1bf40ef2faa96a169682c33
Tim Pakkianathan <tim@pakkianathan.com>
2010-02-27 22:13:01 +1100

    Getting in macros for returning truth of the tests run

--- unit-test-framework.lisp
+++ unit-test-framework.lisp
@@ -1,10 +1,23 @@
 (defun test-+ ()
-  (check (= (+ 1 2) 3))
-  (check (= (+ 1 2 3) 6))
-  (check (= (+ -1 -5) -6)))
+  (check 
+    (= (+ 1 2) 3)
+    (= (+ 1 2 3) 6)
+    (= (+ -1 -5) -6)))
 
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] .... ~a~%" result form))
+  (format t "~:[FAIL~;pass~] .... ~a~%" result form)
+  result)
 
-(defmacro check(form)
-  `(report-result ,form ',form))
+(defmacro check(&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro combine-results (&body forms)
+  (with-gensysms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+
+(defun ping-unit-test-framework (text)
+  (format t "~a~%" text))
