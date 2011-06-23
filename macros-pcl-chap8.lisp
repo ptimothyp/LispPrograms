e45d3a64dc6ffadd8f019ac667a32dcc7bfaa75f
Timothy <timothy@timothy-linux-host.(none)>
2011-06-23 22:52:51 +1000

    Adding the prime macro completed

---
+++ macros-pcl-chap8.lisp
@@ -0,0 +1,22 @@
+(defun primep (number)
+  (when (> number 1)
+    (loop for fac from 2 to (isqrt number) never (zerop (mod number fac)))))
+
+(defun next-prime(number)
+  (loop for n from number when (primep n) return n))
+
+(defun generate-primes(number)
+  (do ((p (next-prime 0) (next-prime (1+ p))))
+	     ((> p number))
+	   (format t "~d " p)))
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro do-primes((var start end) &body body)
+  (with-gensyms(ending-value-name)
+  `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
+	(,ending-value-name ,end))
+      ((> ,var ,ending-value-name))
+    ,@body)))
