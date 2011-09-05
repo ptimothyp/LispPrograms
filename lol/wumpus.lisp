3b395731aca7d60dd2dac415b79f319c102668ea
Tim Pakkianathan <tim@pakkianathan.com>
2011-09-06 09:00:09 +1000

    Adding wumpus game

---
+++ lol/wumpus.lisp
@@ -0,0 +1,26 @@
+(load "/home/timothy/progs/lisp/lol/graph-util")
+
+(defparameter *congestion-city-nodes* nil)
+(defparameter *congeston-city-edges* nil)
+(defparameter *visited-nodes* nil)
+(defparameter *node-num* 30)
+(defparameter *edge-num* 45)
+
+(defparameter *worm-num* 3)
+(defparameter *cop-odds* 15)
+
+(defun random-node ()
+  (1+ (random *node-num*)))
+
+(defun edge-pair (a b)
+  (unless (eql a b)
+    (list (cons a b) (cons b a))))
+
+(defun make-edge-list ()
+  (apply #'append (loop repeat *edge-num*
+                     collect (edge-pair (random-node) (random-node)))))
+
+(defun direct-edges (node edge-list)
+  (remove-if-not (lambda (x)
+                   (eql (car x) node))
+                 edge-list))
