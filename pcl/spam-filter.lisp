71822835aba7ef6d88fc9d39676206af29ba760d
timothy <timothy@timothy-lynx-Desktop.(none)>
2011-09-17 22:33:17 +1000

    Spam filter partial implementation

--- pcl/spam-filter.lisp
+++ pcl/spam-filter.lisp
@@ -1,3 +1,4 @@
+
 (defpackage :com.pakkianathan.spam
   (:use :common-lisp :com.pakkianathan.pathnames))
 
@@ -35,8 +36,6 @@
 
 (defvar *feature-database* (make-hash-table :test #'equal))
 
-(defun clear-database ()
-  (setf *feature-database* (make-hash-table :test #'equal)))
 
 (defun intern-feature (word)
   (or (gethash word *feature-database*)
@@ -50,3 +49,71 @@
 
 (defun extract-features (text)
   (mapcar #'intern-feature (extract-words text)))
+
+(defmethod print-object ((object word-feature) stream)
+  (print-unreadable-object (object stream :type t)
+    (with-slots (word ham-count spam-count) object
+      (format stream "~s :hams ~d :spams ~d" word ham-count spam-count))))
+
+(defun train (text type)
+  (dolist (feature (extract-features text))
+    (increment-count feature type))
+  (increment-total-count type))
+
+(defun increment-count (feature type)
+  (ecase type
+    (ham (incf (ham-count feature)))
+    (spam (incf (spam-count feature)))))
+
+(defvar *total-spams* 0)
+(defvar *total-hams* 0)
+
+(defun increment-total-count (type)
+  (ecase type
+    (ham (incf *total-hams*))
+    (spam (incf *total-spams*))))
+
+(defun clear-database ()
+  (setf
+   *feature-database* (make-hash-table :test #'equal)
+   *total-spams* 0
+   *total-hams* 0))
+
+(defun spam-probability (feature)
+  (with-slots (spam-count ham-count) feature
+    (let ((spam-frequency (/ spam-count (max 1 *total-spams*)))
+          (ham-frequency (/ ham-count (max 1 *total-hams*))))
+      (/ spam-count (+ spam-frequency ham-frequency)))))
+
+(defun bayesian-spam-probability (feature &optional
+                                  (assumed-probability 1/2)
+                                  (weight 1))
+  (let ((basic-probability (spam-probability feature))
+        (data-points (+ (spam-count feature) (ham-count feature))))
+    (/ (+ (* weight assumed-probability)
+          (* data-points basic-probability))
+       (+ weight data-points))))
+
+(defun score (features)
+  (let ((spam-probs ()) (ham-probs ()) (number-of-probs 0))
+    (dolist (feature features)
+      (unless (untrained-p feature)
+        (let ((spam-prob (float (bayesian-spam-probability feature) 0.0d0)))
+          (push spam-prob spam-probs)          
+          (push (- 1.0d0 spam-prob) ham-probs)
+          (incf number-of-probs))))
+    (let ((h (- 1 (fisher spam-probs number-of-probs)))
+          (s (- 1 (fisher ham-probs number-of-probs))))
+      (/ (+ (- 1 h) s) 2.0d0))))
+
+(defun untrained-p (feature)
+  (with-slots (spam-count ham-count) feature
+    (and (zerop spam-count) (zerop ham-count))))
+
+(defun fisher (probs number-of-probs)
+  "The fisher computation described by Robinson"
+  (inverse-chi-square
+   (* -2 (reduce #'+ probs :key #'log))
+   (* 2 number-of-probs)))
+
+
